31b4304283b13a74c895cbae168da6a7d35f3d81
“abhijeetdevani” <ardwork741@gmail.com>
2020-12-02 11:48:48 +0530

    Initial Commit

---
+++ src/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List(){
+    return(
+        <ol>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C</li>
+            <li>C++</li>
+        </ol>
+    );
+}
+
+export default List;
